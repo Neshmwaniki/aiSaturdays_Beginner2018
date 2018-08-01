13c5318168456c628a26e61fdf1b285339d9d80f
Denis Mwaniki <32881577+Neshmwaniki@users.noreply.github.com>
2018-08-01 18:37:17 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,3 +4,10 @@ words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
 randomPhrase
+def reverse(randomPhrase):
+    if len(randomPhrase) == 0:
+        return randomPhrase
+    else:
+        return reverse(randomPhrase[1:]) + randomPhrase[0]
+
+print(reverse(randomPhrase))
